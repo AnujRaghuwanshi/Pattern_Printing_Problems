af883f8c9dafe189b5865f9bcd93151bc634e284
AnujRaghuwanshi <124479521+AnujRaghuwanshi@users.noreply.github.com>
2025-07-25 00:08:11 +0530

    Update Decremental_Inverted_Half_Pyramid.cpp
    C plus plus and Java Implementation

--- Decremental_Inverted_Half_Pyramid.cpp
+++ Decremental_Inverted_Half_Pyramid.cpp
@@ -1,3 +1,13 @@
+Enter value of n
+5
+5 4 3 2 1 
+4 3 2 1 
+3 2 1 
+2 1 
+1 
+
+
+//C++ Program
 #include <iostream>
 using namespace std;
 int main()
@@ -15,3 +25,26 @@ int main()
         cout << endl;
     }
 }
+
+//Java Porgram
+import java.util.Scanner;
+
+public class ReverseNumberPattern {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter value of n:");
+        int n = scanner.nextInt();
+
+        for (int i = n; i >= 1; i--) {
+            for (int j = i; j >= 1; j--) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+
+        scanner.close();
+    }
+}
+
+
