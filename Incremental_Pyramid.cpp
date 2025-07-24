712d262750329a9a9243c19d17f2d9bb3a2462ae
AnujRaghuwanshi <124479521+AnujRaghuwanshi@users.noreply.github.com>
2025-07-25 00:26:10 +0530

    Update Incremental_Pyramid.cpp
    C plus plus and Java Implementation

--- Incremental_Pyramid.cpp
+++ Incremental_Pyramid.cpp
@@ -1,3 +1,13 @@
+Enter value of n
+5
+1 
+1 2 
+1 2 3 
+1 2 3 4 
+1 2 3 4 5 
+
+//CPP Implementation
+    
 #include <iostream>
 using namespace std;
 int main()
@@ -12,5 +22,27 @@ int main()
             cout << j << " ";
         }
         cout << endl;
+    }
+}
+
+//JAVA Implementation
+
+import java.util.Scanner;
+
+public class NumberTriangle {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter value of n:");
+        int n = scanner.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+
+        scanner.close();
     }
 }
