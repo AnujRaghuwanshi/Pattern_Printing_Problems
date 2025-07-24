5cccd4c4cd19638776075ba65af1f3e583d238ef
AnujRaghuwanshi <124479521+AnujRaghuwanshi@users.noreply.github.com>
2025-07-25 00:34:34 +0530

    Update Inverted_Pyramid.cpp
    C plus plus and Java Implementation

--- Inverted_Pyramid.cpp
+++ Inverted_Pyramid.cpp
@@ -1,3 +1,14 @@
+Enter no.of rows
+5
+*********
+ *******
+  *****
+   ***
+    *
+
+
+//CPP Implementation
+
 #include <iostream>
 using namespace std;
 
@@ -9,7 +20,6 @@ int main()
 
     for (int i = row - 1; i >= 0; i--)
     {
-
         for (int k = row - 1; k > i; k--)
         {
             cout << " ";
@@ -22,3 +32,31 @@ int main()
     }
     return 0;
 }
+
+//JAVA Implementation
+
+import java.util.Scanner;
+
+public class InvertedPyramid {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter number of rows:");
+        int row = scanner.nextInt();
+
+        for (int i = row - 1; i >= 0; i--) {
+            for (int k = row - 1; k > i; k--) {
+                System.out.print(" ");
+            }
+
+            for (int j = 0; j < 2 * i + 1; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+
+        scanner.close();
+    }
+}
+
