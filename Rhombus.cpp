f8d3c4ae1d3ae42b5bf87e91a2c83d774c5cc00d
AnujRaghuwanshi <124479521+AnujRaghuwanshi@users.noreply.github.com>
2025-07-25 00:46:15 +0530

    Update Rhombus.cpp
    C plus plus and Java Implementation

--- Rhombus.cpp
+++ Rhombus.cpp
@@ -1,3 +1,12 @@
+Enter no.of rows
+5
+ *****
+  *****
+   *****
+    *****
+     *****
+
+//CPP Implementation
 #include <iostream>
 using namespace std;
 
@@ -10,10 +19,12 @@ int main()
 
     for (int i = 1; i <= rows; i++)
     {
+         // Print leading spaces
         for (int k = 1; k <= i; k++)
         {
             cout << " ";
         }
+        // Print stars in a row
         for (int j = 1; j <= rows; j++)
         {
             cout << "*";
@@ -22,4 +33,30 @@ int main()
     }
 
     return 0;
+}
+
+//JAVA Implementation
+import java.util.Scanner;
+
+public class RightShiftedRectangle {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter number of rows:");
+        int rows = scanner.nextInt();
+
+        for (int i = 1; i <= rows; i++) {
+            // Print leading spaces
+            for (int k = 1; k <= i; k++) {
+                System.out.print(" ");
+            }
+            // Print stars in a row
+            for (int j = 1; j <= rows; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+        scanner.close();
+    }
 }
