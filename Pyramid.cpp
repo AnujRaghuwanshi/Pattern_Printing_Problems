59059d9d71a424c7686d783dc3d64bda18ff2942
AnujRaghuwanshi <124479521+AnujRaghuwanshi@users.noreply.github.com>
2025-07-25 00:44:13 +0530

    Update Pyramid.cpp
    C plus plus and Java Implementation

--- Pyramid.cpp
+++ Pyramid.cpp
@@ -1,3 +1,12 @@
+Enter no.of row
+5
+     *
+    ***
+   *****
+  *******
+ *********
+
+//CPP Implementation    
 #include<iostream>
 using namespace std;
 
@@ -7,14 +16,42 @@ int main(){
     cin>>row;
 
     for(int i=0;i<row;i++){
+        // Print leading spaces
         for(int k=0;k<row-i;k++){
             cout<<" ";
         }
-
+        // Print stars
         for(int j=0;j<2*i+1;j++){
             cout<<"*";
         }
         cout<<endl;
     }
     return 0;
+}
+
+//JAVA Implementation
+import java.util.Scanner;
+
+public class CenteredStarPyramid {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter number of rows:");
+        int row = scanner.nextInt();
+
+        for (int i = 0; i < row; i++) {
+            // Print leading spaces
+            for (int k = 0; k < row - i; k++) {
+                System.out.print(" ");
+            }
+            // Print stars
+            for (int j = 0; j < 2 * i + 1; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+
+        scanner.close();
+    }
 }
